5f3794c54eec15d7fa1c1fa550fbd2503ef0be49
Piotr Świątek <p.swiatek.piotr@gmail.com>
2019-06-27 15:15:28 +0200

    Updates

---
+++ precache-manifest.6ca910b2110b3dc58dd4c421b70f8e55.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b33562b3f97997d27bc4ca0ae2115a8d",
+    "url": "/wedding/index.html"
+  },
+  {
+    "revision": "fe0c8befbfffbaaa6336",
+    "url": "/wedding/static/css/main.ae40b9ca.chunk.css"
+  },
+  {
+    "revision": "cd2efe916af241c9f568",
+    "url": "/wedding/static/js/2.30026ba9.chunk.js"
+  },
+  {
+    "revision": "fe0c8befbfffbaaa6336",
+    "url": "/wedding/static/js/main.7a93dcd2.chunk.js"
+  },
+  {
+    "revision": "f05cdc32ffbcfb27d2df",
+    "url": "/wedding/static/js/runtime~main.8918a5c1.js"
+  },
+  {
+    "revision": "abb695bba98b9d1801c8d9489f79b3c5",
+    "url": "/wedding/static/media/bg.abb695bb.png"
+  },
+  {
+    "revision": "56bdf230b293d82836db447393075734",
+    "url": "/wedding/static/media/confirm-hover.56bdf230.svg"
+  },
+  {
+    "revision": "36402e6cf239e0c96925f4cc869fb6c8",
+    "url": "/wedding/static/media/confirm.36402e6c.svg"
+  },
+  {
+    "revision": "a083c32f20770d2ed4d6846145045b9a",
+    "url": "/wedding/static/media/contact-hover.a083c32f.svg"
+  },
+  {
+    "revision": "79c835305415a132d29a2a8e7b9d58b0",
+    "url": "/wedding/static/media/contact.79c83530.svg"
+  },
+  {
+    "revision": "e3965fa9d6bc6dc62027ea4121700694",
+    "url": "/wedding/static/media/info-hover.e3965fa9.svg"
+  },
+  {
+    "revision": "5363087288f93fe6acf72828663f199d",
+    "url": "/wedding/static/media/info.53630872.svg"
+  },
+  {
+    "revision": "778a41c6db852359d99ef5096e6e2de9",
+    "url": "/wedding/static/media/mail.778a41c6.svg"
+  },
+  {
+    "revision": "5ead419a0852ee8d7537c404f79b9cdf",
+    "url": "/wedding/static/media/phone.5ead419a.svg"
+  }
+]);
