fc54ad453cd26522edbbc013518432d46ddc14bd
Piotr Świątek <p.swiatek.piotr@gmail.com>
2019-06-30 20:43:26 +0200

    Fix a bug - images in ShowImage didn't display after build

--- src/components/ApproachChurch.js
+++ src/components/ApproachChurch.js
@@ -8,20 +8,20 @@ const ApproachChurch = (props) => {
                 <p>
                     Kościół znajduje się przy ulicy Świętego Józefa w Lesznie, w pobliżu wiaduktu, obok Hotelu <i>Akwawit</i> oraz dawnego akwaparku. Poniżej przedstawiamy sposób na dojazd od strony Wschowy.
             </p>
-                <button onClick={() => props.showImage("/location/church1.png")} className="approach-preview"></button>
+                <button onClick={() => props.showImage(require("../img/location/church1.png"))} className="approach-preview"></button>
                 <p>
                     Po wjeździe do Leszna, należy poruszać się na wprost ulicą Szybowników aż do pierwszych świateł (bezpośrednio przed Wiaduktem).
                     Na światłach należy skręcić w lewo.
             </p>
-                <button onClick={() => props.showImage("/location/church2.png")} className="approach-preview"></button>
+                <button onClick={() => props.showImage(require("../img/location/church2.png"))} className="approach-preview"></button>
                 <p>
                     Bezpośrednio po skręcie w lewo, należy zająć prawy pas i skręcić w prawo.
             </p>
-                <button onClick={() => props.showImage("/location/church3.png")} className="approach-preview"></button>
+                <button onClick={() => props.showImage(require("../img/location/church3.png"))} className="approach-preview"></button>
                 <p>
                     Kościół znajduje się na wprost.
             </p>
-                <button onClick={() => props.showImage("/location/church4.png")} className="approach-preview"></button>
+                <button onClick={() => props.showImage(require("../img/location/church4.png"))} className="approach-preview"></button>
             </div>
             <p>
                 Źródło: <cite>Google Maps</cite>.
