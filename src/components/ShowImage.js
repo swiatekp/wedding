aa58212991d3d64c4ad78b7e06adc2c1cfbeec64
Piotr Świątek <p.swiatek.piotr@gmail.com>
2019-07-13 13:12:37 +0200

    Style the image showing component

--- src/components/ShowImage.js
+++ src/components/ShowImage.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import '../css/ShowImage.scss';
-const ShowImage = (props) => {
-    return (
-        <div className="showimage-background">
-            <img src={props.path} alt="obrazek" />)
-            <button className="showimage-close" onClick={props.hideImage}></button>
-        </div>
-    );
+class ShowImage extends React.Component {
+    state = {
+        isZoomed: false
+    }
+    toggleZoom = () => {
+        this.setState(prevState => ({
+            isZoomed: !prevState.isZoomed
+        }));
+    }
+    render = () => {
+        return (
+            <div className="showimage-background">
+                <img className={this.state.isZoomed ? null : "img-not-zoomed"} onClick={this.toggleZoom} src={this.props.path} alt="obrazek" />)
+                <button className="showimage-close-x" onClick={this.props.hideImage}>X</button>
+                <button className="showimage-close-button" onClick={this.props.hideImage}>Zamknij</button>
+            </div>
+        );
+    }
 }
 
 export default ShowImage;
